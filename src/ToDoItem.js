b8bfe0abb2e9cfebffe201daa20688c95096e5f0
EXM <exm@EXMs-MacBook-Pro.local>
2021-07-11 23:09:13 -0400

    my commit

---
+++ src/ToDoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ToDoItems() {
+    return (
+            <div>
+                <input type='checkbox' />
+                <p>Placeholder text</p>
+
+            </div>
+    )
+}
+
+export default ToDoItems
